b78f6a300e33c65d3210e8f07dcc2d5551145fce
NAVEEN171 <naveenkumar171837@gmail.com>
2024-05-11 19:48:39 +0530

    changes made

--- src/components/Appfeatures/Appfeatures.tsx
+++ src/components/Appfeatures/Appfeatures.tsx
@@ -11,17 +11,17 @@ const Appfeatures = () => {
         {
             name:"Personalized Job Matching",
             desc:"Our intelligent algorithm matches you with job opportunities that align with your skills, experience, and career goals",
-            logo:"/image/vector.png"
+            logo:"/image/feature1.png"
         },
         {
             name:"Effortless Application Process",
             desc:"Apply to multiple jobs with just a few clicks, saving you time and effort.",
-            logo:"/image/vector2.png"
+            logo:"/image/feature2.png"
         },
         {
             name:"User-Friendly Interface",
             desc:"Our intuitive interface makes navigating the job search process a breeze, even for beginners.",
-            logo:"/image/vector3.png"
+            logo:"/image/feature3.png"
         }
     ]
   return (
